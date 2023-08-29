5121093b232534d6f3e26b1aba45268d2ba2d0fc
Adriel Chan <adrielchancw@hotmail.com>
2023-08-30 01:30:11 +0800

    prompt feed and get prompts api added.

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+'use client'
+
+import { useState, useEffect } from "react"
+import PromptCard from "./PromptCard"
+
+const PromptCardList = ({ data, handleTagClick }) => {
+    return (
+        // Map callback fn must use () => ()
+        <div className="mt-16 prompt_layout">
+            {data.map((post) => (
+                <PromptCard
+                    key={post._id}
+                    post={post}
+                    handleTagClick={handleTagClick}
+                />
+            ))}
+        </div>
+    )
+}
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
+    const [searchText, setSearchText] = useState('')
+    const handleSearchChange = (e) => {}
+
+    const [posts, setPosts] = useState([]) // add prompts and set them
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch('/api/prompt')
+            const data = await response.json()
+
+            setPosts(data) // updates prompt posts
+        }
+        fetchPosts()
+    }, [])
+    
+
+    return (
+        <section className="feed">
+            <form className="relative w-full flex-center">
+                <input
+                    type="text"
+                    placeholder="Search for a tag or a username"
+                    value={searchText}
+                    onChange={handleSearchChange}
+                    required
+                    className="search_input peer"
+                />
+            </form>
+
+            <PromptCardList
+                data={posts}
+                handleTagClick={() => {}}
+            />
+        </section>
+    )
 }
 
 export default Feed
